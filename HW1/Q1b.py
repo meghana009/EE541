0ba42bea546d11fefde3a2e6154543561fbce930
Meghana Achanta <67195633+meghana009@users.noreply.github.com>
2023-01-14 00:19:56 -0800

    Create Q1b.py

---
+++ HW1/Q1b.py
@@ -0,0 +1,25 @@
+import sys
+import random 
+import matplotlib.pyplot as plt
+import math
+
+#Question 1b for n =20
+N = 50
+p = 0.7
+number_of_heads = 0
+number_of_cont_heads = 0
+n = 20
+freq = []
+
+for j in range(n):
+  for i in range(1, N+1):
+    toss = random.random()
+    if toss < p:
+      number_of_heads += 1
+  freq.append(number_of_heads)
+  number_of_heads = 0
+
+plt.hist(freq)
+
+plt.ylabel('Number of Experiment')
+plt.xlabel('Number of Heads');
